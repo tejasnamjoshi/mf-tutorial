bd2f7990b0ac8b1a1871fe5a73f8ff134f3f2441
tejas-salucro <tnamjoshi@salucro.com>
2023-03-28 12:11:01 +0530

    RD-1234/feat(application) Created a new react application task-breakdown-demo

---
+++ apps/task-breakdown-demo/src/app/app.tsx
@@ -0,0 +1,13 @@
+import './app.scss';
+import NxWelcome from './nx-welcome';
+
+export function App() {
+  return (
+    <>
+      <NxWelcome title="task-breakdown-demo" />
+      <div />
+    </>
+  );
+}
+
+export default App;
